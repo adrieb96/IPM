f9bb13598ac0df3647fa871278e3219b09f56631
adrieb96 <adrieb96@gmail.com>
2016-09-26 22:40:35 +0200

    more

---
+++ p1/movies.py
@@ -0,0 +1,34 @@
+class MyMovies(object):
+
+    def __init__(self):
+        
+        self.movies = []
+
+
+    def getList(self):
+        return self.movies
+
+    def addMovie(self,movie):
+        self.movies.append(movie)
+
+    def deleteMovie(self,movie):
+        self.movies.remove(movie)
+   
+    def updateMovie(self,old,new):
+        self.deleteMovie(old)
+        self.addMovie(new)
+
+class Movie(object):
+
+    def __init__(self,title,time,year):
+        self.title = title
+        self.time = time
+        self.year = year
+    
+    def getData(self):
+        return (self.title,self.time,self.year)
+pelicula = Movie("LotR",200,2000)
+peliculas =  MyMovies()
+peliculas.addMovie(pelicula)
+for movie in peliculas.getList():
+    print movie.getData()
--- p1/sprint1.py
+++ p1/sprint1.py
@@ -1,42 +1,67 @@
 import gi #import gi and force it to acces gtk+3
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
+import movies
+
 
 class MyWindow(Gtk.Window): #create a new class
 
-	def __init__(self): #creates the button
-		Gtk.Window.__init__(self, title="Hello World")
-		self.set_border_width(10)
+    def __init__(self): #creates the button
 
-		hbox = Gtk.Box(spacing=2)
-		self.add(hbox)
+        Gtk.Window.__init__(self, title="Hello World")
+        self.set_border_width(10)
 
-                hboxList = Gtk.Box(spacing=6)
-                hbox.add(hboxList)
-	
-                hboxButton = Gtk.VBox(spacing=20)
-                hbox.add(hboxButton)
+        grid = Gtk.Grid()
+        self.add(grid)
 
-		button = Gtk.Button.new_with_label("Anadir")
-		button.connect("clicked", self.on_anadir)		
-		hboxButton.pack_start(button, True, True, 0)
+        self.peliculas = movies.MyMovies()
+        movieList = Gtk.ListStore(str,int,int)
+        for movie in self.peliculas.getList():
+            movieList.append(list(movie))                    
 
-		button = Gtk.Button.new_with_mnemonic("Borrar")
-		button.connect("clicked", self.on_borrar)
-		hboxButton.pack_start(button, True, True, 0)
-
-                button = Gtk.Button.new_with_mnemonic("Editar")
-                button.connect("clicked", self.on_editar)
-                hboxButton.pack_start(button, True, True, 0)
-
-	def on_anadir(self, button): #define what do when clicked
-		print("Anadir")
-	
-	def on_borrar(self, button):
-		print("Borrar")
+        tree = Gtk.TreeView(movieList)
         
-        def on_editar(self,button):
-                print("Editar")
+        for i, column_title in enumerate(["Titulo", "Duracion", "Ano"]):
+            render = Gtk.CellRendererText()
+            column = Gtk.TreeViewColumn(column_title, render,text=i)
+            tree.append_column(column)
+        
+        #scrollable_treelist = Gtk.ScrolledWindow()
+        #scrollable_treelist.set_vexpand(True)
+        #grid.attach(scrollable_treelist, 0, 0, 8, 10)
+
+
+        grid.add(tree)
+        
+        buttons = Gtk.VBox(spacing=20)
+        
+        grid.attach_next_to(buttons, tree, Gtk.PositionType.RIGHT, 1,1)
+        
+        button = Gtk.Button.new_with_label("Anadir")
+        button.connect("clicked", self.on_anadir)		
+        buttons.pack_start(button, True, True, 0)
+        
+        button = Gtk.Button.new_with_mnemonic("Borrar")
+        button.connect("clicked", self.on_borrar)
+        buttons.pack_start(button, True, True, 0)
+        
+        button = Gtk.Button.new_with_mnemonic("Editar")
+        button.connect("clicked", self.on_editar)
+        buttons.pack_start(button, True, True, 0)
+
+    def on_anadir(self, button):
+        print("Anadir")
+        title = raw_input("title?:")
+        time = raw_input("minutes?:")
+        year = raw_input("year?:")
+        pelicula = movies.Movie(title,time,year)
+        self.peliculas.addMovie(pelicula)
+
+    def on_borrar(self, button):
+        print("Borrar")
+
+    def on_editar(self,button):
+        print("Editar")
 
 win = MyWindow() #creates an empty window
 win.connect("delete-event", Gtk.main_quit) #se asegura de que cierre
