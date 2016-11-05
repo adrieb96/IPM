e0895df76d732b25ae806732eb63ad6b7fe834ca
adrieb96 <adrieb96@gmail.com>
2016-11-05 14:22:06 +0100

    deleting

--- practica2/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/adrian/Documents/GEI/IPM/practica2/bin/AndroidManifest.xml : \
-/home/adrian/Documents/GEI/IPM/practica2/AndroidManifest.xml \
--- practica2/bin/ipm_application-debug-unaligned.apk.d
+++
@@ -1,3 +0,0 @@
-/home/adrian/Documents/GEI/IPM/practica2/bin/ipm_application-debug-unaligned.apk : \
-/home/adrian/Documents/GEI/IPM/practica2/bin/ipm_application.ap_ \
-/home/adrian/Documents/GEI/IPM/practica2/bin/classes.dex \
